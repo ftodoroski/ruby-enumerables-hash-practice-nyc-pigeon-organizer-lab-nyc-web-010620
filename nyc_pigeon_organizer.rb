364c52afa3ffc85e79bf5a0c26d662cae5be4fb4
Filip Todoroski <ftodoroski@ymail.com>
2019-11-24 19:15:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,4 +20,28 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   pigeons = Hash.new{}
 
+  # Makes an empty hash with the pigeons name
+  data[:gender].each do |gender, names|
+    names.each do |pigeon_name|
+      pigeons[pigeon_name] = {}
+      end
+  end
+
+  # Adds attributes to each of the pigeons
+  data.each_key do |attribute|
+    pigeons.each do |name, prop|
+      prop[attribute] = []
+    end
+  end
+
+  # Adds value to the attributes
+  data.each do |attribute, attr_data|
+    attr_data.each do |info, names|
+      names.each do |name|
+        pigeons[name][attribute] << info
+      end
+    end
+  end
+
+  pigeons
 end
